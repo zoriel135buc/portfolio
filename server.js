010b30847ecab994abfe1786099a054731f4b0a4
zoriel135buc <zorielbuc135@gmail.com>
2024-04-30 15:20:04 +0300

    first commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const usersRouter = require("./routes/userRoutes.js");
 const app = express();
-
+const path = require("path");
 const corsOptions = {
   origin: "http://localhost:5173",
   methods: ["GET", "POST"],
@@ -19,3 +19,9 @@ app.listen(process.env.PORT || 3001, () => {
 });
 
 app.use("/users", usersRouter);
+app.use(express.static(path.join(__dirname, "client/portfolio/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(
+    path.resolve(__dirname, "./client/portfolio/dist", "index.html")
+  );
+});
