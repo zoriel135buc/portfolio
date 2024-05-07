50b4797ff53f8236efd55b06074e28f873e9083d
zoriel135buc <zorielbuc135@gmail.com>
2024-05-07 14:09:13 +0300

    update

--- client/portfolio/src/Components/Post.jsx
+++ client/portfolio/src/Components/Post.jsx
@@ -4,6 +4,7 @@ import Box from "@mui/material/Box";
 import { useState } from "react";
 import TextField from "@mui/material/TextField";
 import { Button } from "@mui/material";
+import post from "../assets/post.jpg";
 
 const Post = ({ page }) => {
   const [phone, setPhone] = useState("");
@@ -20,8 +21,8 @@ const Post = ({ page }) => {
         {
           email,
           message,
-          firstname, // Corrected from 'firstname'
-          lastname, // Corrected from 'lastname'
+          firstname,
+          lastname,
           phone,
         },
         {
@@ -39,77 +40,73 @@ const Post = ({ page }) => {
   };
 
   return (
-    <div>
-      <h2>Contact</h2>
-      <Box
-        component={"form"}
-        sx={{
-          m: 20,
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-        autoComplete="off"
-      >
-        <TextField
-          sx={{ m: 1 }}
-          id="email"
-          type="email"
-          label="Enter your email"
-          variant="outlined"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <TextField
-          sx={{ m: 1 }}
-          id="message"
-          type="text"
-          label="Enter your message"
-          variant="outlined"
-          onChange={(e) => setMessage(e.target.value)}
-        />
-        <TextField
-          sx={{ m: 1 }}
-          id="phone"
-          type="text"
-          label="Enter your phone"
-          variant="outlined"
-          onChange={(e) => setPhone(e.target.value)}
-        />
-        <TextField
-          sx={{ m: 1 }}
-          id="firstname"
-          type="text"
-          label="Enter your first name"
-          onChange={(e) => setFirstName(e.target.value)}
-        />
-        <TextField
-          sx={{ m: 1 }}
-          id="lastname"
-          type="text"
-          label="Enter your last name"
-          onChange={(e) => setLastName(e.target.value)}
-        />
-      </Box>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          marginTop: "10px",
-        }}
-      >
-        <Button
+    <div className="post" style={{ backgroundImage: `url(${post})` }}>
+      <div className="postP">
+        <h2>Contact</h2>
+        <Box
+          component={"form"}
           sx={{
-            alignItems: "center",
+            m: 20,
             display: "flex",
-            justifyContent: "center",
             flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            margin: "30px",
           }}
-          onClick={PostDea}
-          variant="contained"
+          autoComplete="off"
         >
-          Post
-        </Button>
+          <TextField
+            sx={{ m: 1 }}
+            id="email"
+            type="email"
+            label="Enter your email"
+            variant="outlined"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <TextField
+            sx={{ m: 1 }}
+            id="message"
+            type="text"
+            label="Enter your message"
+            variant="outlined"
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <TextField
+            sx={{ m: 1 }}
+            id="phone"
+            type="text"
+            label="Enter your phone"
+            variant="outlined"
+            onChange={(e) => setPhone(e.target.value)}
+          />
+          <TextField
+            sx={{ m: 1 }}
+            id="firstname"
+            type="text"
+            label="Enter your first name"
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+          <TextField
+            sx={{ m: 1 }}
+            id="lastname"
+            type="text"
+            label="Enter your last name"
+            onChange={(e) => setLastName(e.target.value)}
+          />
+          <Button
+            sx={{
+              alignItems: "center",
+              display: "flex",
+              justifyContent: "center",
+              flexDirection: "column",
+            }}
+            onClick={PostDea}
+            variant="contained"
+            style={{ marginBottom: "10px" }}
+          >
+            Post
+          </Button>
+        </Box>
       </div>
       <div>{err}</div>
     </div>
